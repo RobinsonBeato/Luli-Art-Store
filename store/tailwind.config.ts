8f45211456ce1442561a87389f6d7331de0cae70
RobinsonBeato <robinsonbeato95@gmail.com>
2024-09-12 11:16:13 -0300

    Using prisma and crtating admin dashboard

--- store/tailwind.config.ts
+++ store/tailwind.config.ts
@@ -1,14 +1,18 @@
 import type { Config } from "tailwindcss";
+import {fontFamily} from "tailwindcss/defaultTheme";
 
 const config: Config = {
     darkMode: ["class"],
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
+		fontFamily: {
+			sans: ["var(--font-sans)", ...fontFamily.sans],
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
